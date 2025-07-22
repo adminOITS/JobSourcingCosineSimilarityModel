c6f48b6c2d93439d75090a26d79616d47ecbf35a
its_rahmoun <rahmoun.fst@uhp.ac.ma>
2025-07-22 16:18:27 +0200

    adding logs

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ def lambda_handler(event, context):
             )
         return {
             "statusCode": 200,
-            "body": json.dumps(result)
+            "body": json.dumps(result, default=str)
         }
         
 
